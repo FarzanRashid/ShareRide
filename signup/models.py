724122e4d6d55c5f0e5f1daf50bc9ae99f24767c
FarzanRashid <promit666@gmail.com>
2024-04-16 17:39:08 +0600

    Move model to correct file

--- signup/models.py
+++ signup/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Users(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    date = models.DateField(auto_now_add=True)
+    time = models.TimeField(auto_now_add=True)
+    password = models.CharField(max_length=128)
+
+    class Meta:
+        db_model = 'Users'
