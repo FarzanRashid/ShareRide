4af0c05a39f77f166dd3fc373dbd7f9c2c0b6b8a
FarzanRashid <promit666@gmail.com>
2024-04-16 17:28:44 +0600

    Add model for signup

--- signup/views.py
+++ signup/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.db import models
 
-# Create your views here.
+
+class Users(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    date = models.DateField(auto_now_add=True)
+    time = models.TimeField(auto_now_add=True)
+    password = models.CharField(max_length=128)
+
+    class Meta:
+        db_model = 'Users'
