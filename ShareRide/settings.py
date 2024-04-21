e0459a2e209b85f1c51b65e0e685741cbc11040c
FarzanRashid <promit666@gmail.com>
2024-04-21 21:43:11 +0600

    Configure database

--- ShareRide/settings.py
+++ ShareRide/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'ShareRide.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'shareride',
+        'USER': 'postgres',
+        'PASSWORD': '87654321it',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
