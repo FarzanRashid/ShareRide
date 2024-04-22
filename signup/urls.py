084c94e748b42f8d1f44ad531fb5b283cec32513
FarzanRashid <promit666@gmail.com>
2024-04-22 22:23:16 +0600

    Add url for redirecting

--- signup/urls.py
+++ signup/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import signup
+from .views import signup, signup_success
 
 urlpatterns = [
     path('', signup, name='signup'),
+    path('success/', signup_success, name='signup_success'),
 ]
