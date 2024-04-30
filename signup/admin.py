a7d8554bd0e07c96c4a5f600dda3f579724cfcda
FarzanRashid <promit666@gmail.com>
2024-04-30 19:37:34 +0600

    Add UsersModelAdmin class

--- signup/admin.py
+++ signup/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
 from .models import Users
 
-admin.site.register(Users)
+
+class UsersModelAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'email', 'get_date_created']
+
+    def get_date_created(self, obj):
+        return obj.date
+
+    get_date_created.short_description = 'Date Created'
+
+
+admin.site.register(Users, UsersModelAdmin)
