62991c2e268aa06b02433827c75f4805802c9cc8
FarzanRashid <promit666@gmail.com>
2024-04-30 12:17:30 +0600

    Add view for login page

--- login/views.py
+++ login/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+import jwt
+from django.conf import settings
+from .forms import LoginForm
 
-# Create your views here.
+
+def login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            token = jwt.encode(
+                {'email': email},
+                settings.SECRET_KEY,
+                algorithm='HS256'
+            ).decode('utf-8')
+            response = redirect(reverse('login_success'))
+            response.set_cookie('jwt', token, httponly=True)
+            return response
+        else:
+            return render(request, 'login.html', {'form': form, 'error': form.errors})
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {'form': form})
