955bfdba89dad716cddc9863c26f5af3bbbf1e95
FarzanRashid <promit666@gmail.com>
2024-04-30 12:30:00 +0600

    Add url for login success page

--- login/urls.py
+++ login/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import login
+from .views import login, login_success
 
 urlpatterns = [
     path('', login, name='login'),
+    path("success/", login_success, name='login_success')
 ]
