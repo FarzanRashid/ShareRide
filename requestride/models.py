4c8f5ad810df91010b29bc8ee31a91318cfe41f6
FarzanRashid <promit666@gmail.com>
2024-05-09 18:15:10 +0600

    Add model

--- requestride/models.py
+++ requestride/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from signup.models import Users
 
-# Create your models here.
+
+class Requests(models.Model):
+    pickup = models.CharField(max_length=100)
+    destination = models.CharField(max_length=100)
+    time = models.TimeField()
+    date = models.DateField(auto_now_add=True)
+    user = models.ForeignKey('signup.Users', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'Requests'
+        verbose_name_plural = "Requests"
