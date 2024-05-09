9a4de29571a5153e6a4d2121c03e0090f8d12843
FarzanRashid <promit666@gmail.com>
2024-05-09 21:47:38 +0600

    Add view for profile page

--- login/views.py
+++ login/views.py
@@ -5,6 +5,7 @@ from django.conf import settings
 from .forms import LoginForm
 from django.http import HttpResponseForbidden
 from signup.models import Users
+from requestride.models import Requests
 
 
 def login(request):
@@ -28,3 +29,18 @@ def login(request):
         form = LoginForm()
         return render(request, 'login.html', {'form': form})
 
+
+def profile(request):
+    jwt_token = request.COOKIES.get('jwt')
+    if jwt_token:
+        try:
+            decoded_token = jwt.decode(jwt_token, settings.SECRET_KEY, algorithms=['HS256'])
+            email = decoded_token['email']
+            user = Users.objects.get(email=email)
+            pending_requests = Requests.objects.filter(user=user, status='pending')
+            finished_requests = Requests.objects.filter(user=user, status='finished')
+            return render(request, 'profile.html', {'pending_requests': pending_requests,
+                                                    'finished_requests': finished_requests})
+        except jwt.InvalidTokenError:
+            return HttpResponseForbidden("Invalid token")
+    return redirect(reverse('login'))
