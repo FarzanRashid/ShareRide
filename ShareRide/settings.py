0c46f25f7f5023ead46aa70b3e55764250bf310b
FarzanRashid <promit666@gmail.com>
2024-05-19 02:38:40 +0600

    Configure celery

--- ShareRide/settings.py
+++ ShareRide/settings.py
@@ -134,3 +134,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CELERY_BROKER_URL = 'amqp://guest:guest@localhost:5672//'
+CELERY_TIMEZONE = "Asia/Dhaka"
+CELERY_IMPORTS = ('requestride.tasks',)
